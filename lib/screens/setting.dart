fdb7eb36e93ad2dbec7ba47d7f28f0a9d8f27a84
Jay Shah <21it142@charusat.edu.in>
2024-06-10 15:57:15 +0530

    comments added

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+//import 'package:get/get.dart';
 import 'package:pressdata/screens/main_page.dart';
 
 class Setting1 extends StatefulWidget {
